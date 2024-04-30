6692554f3035cc71b00eb00c4d84fd615f7defd2
Obison360 <jbennjoku@gmail.com>
2024-04-30 13:46:49 +0100

    new learning

--- Phython/Microprogramming.py
+++ Phython/Microprogramming.py
@@ -2,3 +2,24 @@
 weight = {"a":10, "b":20, "c":30, "d":40, "e":50, "f":60, "g":70}
 print (type(weight))
 print(weight["c"])
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
