bbd7731b0f8f5bc7f2c62e2dc563b4ec0f66770f
Obison360 <jbennjoku@gmail.com>
2024-05-16 20:42:57 +0100

    new

--- Phython/Micro_cl3.py
+++ Phython/Micro_cl3.py
@@ -1,4 +1,16 @@
 x = 1
 print (type(x))
 print (type("Hello"))
-print (x + "Hello")
+# print (x + "Hello") {Isn't correct because the classes are different (int and string)}
+
+#Creating a Class
+class Dog:
+    def bark(self):
+        print ("bark")
+
+d = Dog()
+print (type(d))
+
+
+
+#Stack Data structure
